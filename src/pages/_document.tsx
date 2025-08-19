9999237e19e283debdac694a02328c13d88bebc3
Halim <halimputra0701@gmail.com>
2025-08-19 12:16:13 +0700

    refactor(page): update meta content

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -21,7 +21,7 @@ export default function Document() {
         <meta name="theme-color" content="#ffffff" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
-        <meta name="apple-mobile-web-app-title" content="Clean Neubrutalism" />
+        <meta name="apple-mobile-web-app-title" content="Clean Neobrutalism" />
         <meta name="mobile-web-app-capable" content="yes" />
       </Head>
       <body className="bg-white text-zinc-950">
